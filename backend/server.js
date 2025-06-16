2a1db6d147f73650e6e7dd5c1605c0f54e2fb466
POOJA <poojabhat2004@gmail.com>
2025-06-16 23:11:58 +0530

    added chat routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db")
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const dotenv = require("dotenv");
 
 const app= express();
@@ -15,6 +16,7 @@ app.get('/',(req,res)=>{
 connectDB();
 
 app.use('/api/users',userRoutes);
+app.use('/api/chat',chatRoutes);
 
 const server = app.listen(5000,()=>{console.log('Server running at PORT:5000')});
 
